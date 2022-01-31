fb7f6121a1767267c52f0f35b0d4a1898656cfe7
Ranjana Reghunath <ranjanareghunath.mec@gmail.com>
2022-01-31 11:14:06 +0530

    higher order functions

--- higherOrderFunctions.js
+++ higherOrderFunctions.js
@@ -1,23 +1,66 @@
-const forEachNumbers = [5, 10, 15, 20, 25];
+const numbers = [5, 10, 15, 20, 25];
 
-function double(array) {
+//Map--------------
+const map = (array, mapFunc) => {
+  let mapResult = [];
   for (let i = 0; i < array.length; i++) {
-    console.log("Using forEach", array[i] * 2);
+    mapResult.push(mapFunc(array[i]));
   }
-}
+  console.log(mapResult);
+};
 
-double(forEachNumbers);
+const double = (num) => {
+  newNum = num * 2;
+  return newNum;
+};
 
-const filterNumbers = [1, 2, 3, 4, 5];
-resultArray = [];
-function isOdd(array) {
+map(numbers, double);
+
+//Filter-----------------------------------------------
+const filter = (array, filterFunc) => {
+  let filterResult = [];
   for (let i = 0; i < array.length; i++) {
-    if (array[i] % 2 !== 0) {
-      resultArray.push(array[i]);
+    if (filterFunc(array[i])) {
+      filterResult.push(array[i]);
     }
   }
-  return resultArray;
-}
+  console.log(filterResult);
+};
 
-const resultArray = isOdd(filterNumbers);
-console.log(oddArray);
+const isOdd = (num) => {
+  return num % 2 == 0;
+};
+
+filter(numbers, isOdd);
+
+//Reduce--------------------------------------------
+
+const reduce = (array, reduceFunc) => {
+  initialValue = [0];
+  for (let i = 0; i < array.length; i++) {
+    reduceFunc(array[i], initialValue);
+  }
+  console.log(initialValue);
+};
+
+const sumOfArray = (num, initialValue) => {
+  initialValue[0] += num;
+  return initialValue;
+};
+
+reduce(numbers, sumOfArray);
+
+//forEach-------------------------------------------
+
+const forEach = (array, forEachFunc) => {
+  for (let i = 0; i < array.length; i++) {
+    console.log(forEachFunc(array[i]));
+  }
+};
+
+const addFive = (num) => {
+  newNum = num + 5;
+  return newNum;
+};
+
+forEach(numbers, addFive);
